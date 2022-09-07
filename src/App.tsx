c7fb825c194e2d433cd6bc19b5377842f3866faf
Cristian Rosca <cristianrrosca@gmail.com>
2022-09-07 16:03:19 +0100

    add ToDo model

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import internal from 'stream';
 import './App.css';
 import InputField from './components/InputField';
+import { ToDo } from './model';
 
 
 
 const App: React.FC = () => {
 
   const [toDo, setToDo] = useState<string>(" ");
+  const [toDoList, setToDoList] = useState<ToDo[]>([]);
 
   console.log(toDo);
   
